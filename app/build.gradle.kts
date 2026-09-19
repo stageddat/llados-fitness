d4c3b08061288be8756eda69acc1fce51bce9700
stageddat <hello@stageddat.dev>
2026-09-19 23:53:44 +0200

    feat: add title and name card

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ android {
 dependencies {
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.cardview)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
